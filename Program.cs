473f47e8a0735c0b524b39978f9b63ec3d9e36ed
FABRICIO <fnmamao@gmail.com>
2020-03-02 21:46:14 -0300

    adiciona a box

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _Box_exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("| Fabricio  |");
+            Console.WriteLine("+-----------+");
+        }
+    }
+}
